1a8e6fc38733c9551ca152babaf6e440f2c6be70
Magaliburstein <mburstein@fi.uba.ar>
2024-06-04 00:47:43 -0300

    feat: US14: 01 corregimos los steps

--- features/step_definitions/consultar_lista_steps.rb
+++ features/step_definitions/consultar_lista_steps.rb
@@ -10,12 +10,15 @@ Dado('que existe un usuario con email "fer@gmail.com" y telegram_id 123456789') 
 end
 
 Dado('que existen 3 contenidos en la plataforma') do
+  @nombre_pelicula1 = 'Nahir'
   request_body1 = { titulo: 'Nahir', anio: 2024, genero: 'drama' }.to_json
   @response_pelicula1 = Faraday.post('/contenido', request_body1, { 'Content-Type' => 'application/json' })
 
+  @nombre_pelicula2 = 'Amor'
   request_body2 = { titulo: 'Amor', anio: 2001, genero: 'comedia' }.to_json
   @response_pelicula2 = Faraday.post('/contenido', request_body2, { 'Content-Type' => 'application/json' })
 
+  @nombre_pelicula3 = 'Batman'
   request_body3 = { titulo: 'Batman', anio: 1998, genero: 'accion' }.to_json
   @response_pelicula3 = Faraday.post('/contenido', request_body3, { 'Content-Type' => 'application/json' })
 end
@@ -63,7 +66,7 @@ end
 Entonces('se ve una lista de los 3 contenidos mas vistos') do
   json_response = JSON.parse(@response.body)
 
-  expect(json_response[0]['id_pelicula']).to eq @id_pelicula1
-  expect(json_response[1]['id_pelicula']).to eq @id_pelicula2
-  expect(json_response[2]['id_pelicula']).to eq @id_pelicula3
+  expect(json_response[0]['nombre_pelicula']).to eq @nombre_pelicula1
+  expect(json_response[1]['nombre_pelicula']).to eq @nombre_pelicula2
+  expect(json_response[2]['nombre_pelicula']).to eq @nombre_pelicula3
 end
