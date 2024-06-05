17c12d3f99b8da6753878c86d4d18b15810230ec
Magaliburstein <mburstein@fi.uba.ar>
2024-06-05 01:24:14 -0300

    refactor US15: 01 corregimos los steps

--- features/step_definitions/calificar_contenido_step.rb
+++ features/step_definitions/calificar_contenido_step.rb
@@ -5,6 +5,14 @@ Dado('que el usuario ya vio la pelicula {string} {int} {string}') do |titulo, an
   request_body = { titulo:, anio:, genero: }.to_json
   @response_pelicula = Faraday.post('/contenido', request_body, { 'Content-Type' => 'application/json' })
   @id_pelicula = JSON.parse(request_body)['id']
+
+  json_response_usuario = JSON.parse(@response_usuario.body)
+  @email = json_response_usuario['email']
+  @id_telegram = json_response_usuario['id']
+
+  @fecha = Time.now.floor.iso8601
+  request_body = { email: @email, id_pelicula: @id_pelicula, fecha: @fecha }.to_json
+  @response = Faraday.post('/visualizacion', request_body, { 'Content-Type' => 'application/json' })
 end
 
 # Cuando
@@ -13,7 +21,7 @@ end
 Cuando('califica la pelicula con un {int}') do |calificacion|
   @calificacion = calificacion
 
-  request_body = { id_pelicula: @id_pelicula, calificacion: }.to_json
+  request_body = { id_usuario: @id_telegram, id_pelicula: @id_pelicula, calificacion: }.to_json
   @response_calificacion = Faraday.post('/calificacion', request_body, { 'Content-Type' => 'application/json' })
 end
 
@@ -26,6 +34,7 @@ Entonces('ve un mensaje de exito') do
   json_response = JSON.parse(@response_calificacion.body)
 
   expect(json_response['id']).to be > 0
+  expect(json_response['id_usuario']).to eq @id_telegram
   expect(json_response['id_pelicula']).to eq @id_pelicula
   expect(json_response['calificacion']).to eq @calificacion
 end
