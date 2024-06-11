15221c3cad29f04995cfd453c345995d1981762b
Bruno Starnone <bstarnone@fi.uba.ar>
2024-06-11 13:55:27 -0300

    feat US23: Escenario negativo agregado. Un contenido en la plataforma que no fue marcado como favorito, no se lista

--- features/step_definitions/listar_favoritos_step.rb
+++ features/step_definitions/listar_favoritos_step.rb
@@ -24,6 +24,20 @@ Dado('que marco las películas {string}, {string} y {string} como favoritas') do
   Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
 end
 
+Dado('que marco las películas {string} y {string} como favoritas') do |_string, _string2|
+  json_response_usuario = JSON.parse(@response_usuario.body)
+  @id_telegram = json_response_usuario['id_telegram']
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_pelicula1 }.to_json
+  Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_pelicula2 }.to_json
+  Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
+end
+
+Dado('que no marco {string} como favorita') do |string|
+end
+
 # Cuando
 # =========================================================
 Cuando('quiero ver mis favoritos') do
@@ -47,4 +61,12 @@ Entonces('aparece {string}, {string} y {string} en el listado') do |titulo1, tit
   expect(@favoritos[0]['titulo']).to eq titulo2
   expect(@favoritos[1]['titulo']).to eq titulo3
   expect(@favoritos[2]['titulo']).to eq titulo1
+end
+
+Entonces('aparece {string} y {string} en el listado') do |titulo1, titulo2|
+  @favoritos = JSON.parse(@response_favoritos.body)
+  expect(@response_favoritos.status).to eq 200
+  expect(@favoritos.length).to eq 2
+  expect(@favoritos[0]['titulo']).to eq titulo1
+  expect(@favoritos[1]['titulo']).to eq titulo2
 end
