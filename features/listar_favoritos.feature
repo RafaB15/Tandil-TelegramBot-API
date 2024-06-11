f85fe6a94c350708d83fd0bf470fde8d62b4af00
Bruno Starnone <bstarnone@fi.uba.ar>
2024-06-11 00:47:39 -0300

    feat US23: Escenario 01 pasando. Se lista correctamente cuando hay un favorito

--- features/listar_favoritos.feature
+++ features/listar_favoritos.feature
@@ -6,8 +6,7 @@ Antecedentes:
   Dado que existe un usuario con email "fer@gmail.com" y id_telegram 123456789
   Dado que existen 3 contenidos en la plataforma
   Dado que hay 3 contenidos vistos en la plataforma
-
-@wip
+  
 Escenario: US23 - 01 Lista de favoritos con una sola pelicula
  
     Dado que marco la película "Batman" como favorita
@@ -17,6 +16,6 @@ Escenario: US23 - 01 Lista de favoritos con una sola pelicula
 @wip
 Escenario: US23 - 01 Lista de favoritos con 3 peliculas
      
-    Dado que marco la película "Batman", "Nahir" y "Amor" como favoritas
+    Dado que marco las películas "Batman", "Nahir" y "Amor" como favoritas
     Cuando quiero ver mis favoritos
     Entonces aparece "Batman", "Nahir" y "Amor" en el listado
--- features/step_definitions/listar_favoritos_step.rb
+++ features/step_definitions/listar_favoritos_step.rb
@@ -10,8 +10,18 @@ Dado('que marco la película {string} como favorita') do |_string|
   @response = Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
 end
 
-Dado('que marco la película {string}, {string} y {string} como favoritas') do |_string, _string2, _string3|
-  pending # Write code here that turns the phrase above into concrete actions
+Dado('que marco la películas {string}, {string} y {string} como favoritas') do |_titulo1, _titulo2, _titulo3|
+  json_response_usuario = JSON.parse(@response_usuario.body)
+  @id_telegram = json_response_usuario['id_telegram']
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_pelicula1 }.to_json
+  Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_pelicula2 }.to_json
+  Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_pelicula3 }.to_json
+  Faraday.post('/favorito', request_body, { 'Content-Type' => 'application/json' })
 end
 
 # Cuando
@@ -30,6 +40,9 @@ Entonces('aparece {string} en el listado') do |titulo|
   expect(@favoritos[0]['titulo']).to eq titulo
 end
 
-Entonces('aparece {string}, {string} y {string} en el listado') do |_string, _string2, _string3|
-  pending # Write code here that turns the phrase above into concrete actions
+Entonces('aparece {string}, {string} y {string} en el listado') do |_titulo1, _titulo2, _titulo3|
+  @favoritos = JSON.parse(@response_favoritos.body)
+  expect(@response_favoritos.status).to eq 200
+  expect(@favoritos.length).to eq 1
+  expect(@favoritos[0]['titulo']).to eq titulo
 end
