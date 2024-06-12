d36e945cf5eeeab9a5b784d0fc3f6cc7e9f92fc6
Magaliburstein <mburstein@fi.uba.ar>
2024-06-12 17:51:00 -0300

    feat US15.2: 02 agregamos los pasos

--- features/calificar_contenido.feature
+++ features/calificar_contenido.feature
@@ -13,19 +13,16 @@ Antecedentes:
     Escenario: US15 - 02 Como cinefilo quiero poder re-calificar contenido ya visto
         Dado que el usuario la habia calificado con un 5
         Cuando califica la pelicula con un 3 se actualiza
-        Y ve un mensaje "Calificacion registrada exitosamente"
+        Entonces ve un mensaje que el la calificacion fue actualizada
 
-    @wip
     Escenario: US15 - 03 Ingreso el valor de la calificacion negativa
         Cuando califica la pelicula con un -1
         Entonces ve un mensaje de calificacion fuera de rango
 
-    @wip
     Escenario: US15 - 04 El contenido a calificar no existe
         Cuando el usuario quiere calificar con 4 un contenido que no existe en la base de datos
         Entonces ve un mensaje de que el contenido a calificar no existe
 
-    @wip
     Escenario: US15 - 05 La calificacion esta fuera de rango
         Cuando califica la pelicula con un 6
         Entonces ve un mensaje de calificacion fuera de rango
--- features/step_definitions/calificar_contenido_step.rb
+++ features/step_definitions/calificar_contenido_step.rb
@@ -25,6 +25,13 @@ Cuando('califica la pelicula con un {int}') do |calificacion|
   @response_calificacion = Faraday.post('/calificacion', request_body, { 'Content-Type' => 'application/json' })
 end
 
+Cuando('el usuario quiere calificar con {int} un contenido que no existe en la base de datos') do |calificacion|
+  @calificacion = calificacion
+
+  request_body = { id_telegram: @id_telegram, id_pelicula: 11_111_111, calificacion: }.to_json
+  @response_calificacion = Faraday.post('/calificacion', request_body, { 'Content-Type' => 'application/json' })
+end
+
 # Entonces
 # =========================================================
 
@@ -47,9 +54,27 @@ Entonces('ve un mensaje de que el contenido a calificar no existe') do
   expect(@response_calificacion.status).to eq 404
 end
 
-Cuando('el usuario quiere calificar con {int} un contenido que no existe en la base de datos') do |calificacion|
+Dado('que el usuario la habia calificado con un {int}') do |calificacion|
   @calificacion = calificacion
 
-  request_body = { id_telegram: @id_telegram, id_pelicula: 11_111_111, calificacion: }.to_json
+  request_body = { id_telegram: @id_telegram, id_pelicula: @id_pelicula, calificacion: }.to_json
   @response_calificacion = Faraday.post('/calificacion', request_body, { 'Content-Type' => 'application/json' })
+end
+
+Cuando('califica la pelicula con un {int} se actualiza') do |nueva_calificacion|
+  @nueva_calificacion = nueva_calificacion
+
+  request_body = { id_telegram: @id_telegram, id_pelicula: @id_pelicula, calificacion: nueva_calificacion }.to_json
+  @response_calificacion_actualizada = Faraday.put('/calificacion', request_body, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('ve un mensaje que el la calificacion fue actualizada') do
+  expect(@response_calificacion_actualizada.status).to eq 201
+
+  json_response = JSON.parse(@response_calificacion_actualizada.body)
+
+  expect(json_response['id']).to be > 0
+  expect(json_response['id_telegram']).to eq @id_telegram
+  expect(json_response['id_pelicula']).to eq @id_pelicula
+  expect(json_response['calificacion']).to eq @nueva_calificacion
 end
