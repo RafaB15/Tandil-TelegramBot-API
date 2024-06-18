259478df4ff5b6e1a8cc6e7bf4f2da6d65c3ccda
rafab15 <rafaelberenguel15@gmail.com>
2024-06-18 00:56:35 -0300

    feat US28.1: Un usuario puede calificar una temporada de una serie

---
+++ features/step_definitions/calificar_temporada_de_serie_step.rb
@@ -0,0 +1,38 @@
+# Dado
+# =========================================================
+
+Dado('que el usuario ya vio la temporada de serie') do
+  @id_contenido = JSON.parse(@response_temporada.body)['id']
+
+  json_response_usuario = JSON.parse(@response_usuario.body)
+  email = json_response_usuario['email']
+  @id_telegram = json_response_usuario['id_telegram']
+
+  fecha = Time.now.floor.iso8601
+  request_body = { email:, fecha: }.to_json
+  Faraday.post("/contenidos/#{@id_contenido}/visualizaciones", request_body, { 'Content-Type' => 'application/json' })
+end
+
+# Cuando
+# =========================================================
+
+Cuando('califica la temporada de serie con un {int}') do |int|
+  @puntaje = int
+
+  request_body = { id_telegram: @id_telegram, id_contenido: @id_contenido, puntaje: @puntaje }.to_json
+  @response_calificaciones = Faraday.post('/calificaciones', request_body, { 'Content-Type' => 'application/json' })
+end
+
+# Entonces
+# =========================================================
+
+Entonces('ve un mensaje de calificacion exitosa') do
+  expect(@response_calificaciones.status).to eq 201
+
+  json_response = JSON.parse(@response_calificaciones.body)
+
+  expect(json_response['id']).to be > 0
+  expect(json_response['id_telegram']).to eq @id_telegram
+  expect(json_response['id_contenido']).to eq @id_contenido
+  expect(json_response['puntaje']).to eq @puntaje
+end
--- lib/version.rb
+++ lib/version.rb
@@ -1,7 +1,7 @@
 class Version
   MAYOR = 2
   MINOR = 0
-  PATCH = 3
+  PATCH = 4
 
   def self.current
     "#{MAYOR}.#{MINOR}.#{PATCH}"
